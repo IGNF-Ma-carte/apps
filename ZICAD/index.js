968dffd893de368881323c368c514858b65dd02d
viglino <jean-marc.viglino@ign.fr>
2025-04-28 08:34:44 +0200

    [BUG] multi maps

--- ZICAD/index.js
+++ ZICAD/index.js
@@ -4,6 +4,6 @@ MapIFrameAPI.ready('map', function(api) {
   // Récupération de l'API pour un accès global
   mapAPI = api;
   // Afficher les layers dans la console
-  api.getLayers(console.log)
+  api.getLayers('OK', console.log)
   api.addEditBar({ layerId: 10, tools: ['Select','DrawPolygon','Attributes','Delete','Export','Import'] })
 })
