bb9e2b9c792574543306513357380d0a860187dd
viglino <jean-marc.viglino@ign.fr>
2025-06-25 10:28:07 +0200

    RNB

---
+++ RNB/rnb.js
@@ -0,0 +1,44 @@
+const search = document.querySelector('#volet input[type="search"]')
+search.addEventListener('change', function(e) {
+  fetch('https://rnb-api.beta.gouv.fr/api/alpha/buildings/'+e.target.value+'/').then(resp => resp.json()).then(function(resp) {
+    mapAPI.setCenter({ center: resp.point.coordinates, zoom:18 })
+    console.log('center', resp)
+    setTimeout(function() {
+      mapAPI.selectFeatures({
+        layerId: 4,
+        attr: 'identifiants_rnb',
+        op: 'contain',
+        val: resp.rnb_id
+      })
+    }, 500)
+  })
+})
+
+/* Get mapAPI */
+var mapAPI;
+MapIFrameAPI.ready('map', function(api) {
+  // Récupération de l'API pour un accès global
+  mapAPI = api;
+  // Afficher les layers dans la console
+  api.on('select', function(e) {
+    var ul = document.querySelector('.result')
+    ul.innerHTML = ''
+    var f = e[0]
+    if (f) {
+      f.properties.identifiants_rnb.split('/').forEach(element => {
+        var li = document.createElement('li');
+        ul.appendChild(li)
+        var a = document.createElement('a');
+        a.innerText = element;
+        a.href = 'javascript:find("'+element+'")';
+        li.appendChild(a)
+      });
+      // search.value = f.properties.identifiants_rnb.split('/')[0];
+    }
+  })
+})
+
+function find(x) {
+  search.value = x;
+  search.dispatchEvent(new Event('change'))
+}
